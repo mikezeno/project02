444ae51bb8cf0c7959eabeeaa0d2943e80be4930
Mike <mzeno184@gmail.com>
2020-07-17 18:04:28 -0400

    Bug fixes and Improvements
    Version 1.2.0
    
    -Fixed logo alignment
    -Styling fixes
    -Added Google font import
    -Added Number of GIFs returned
    -Added media queries

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     $("#searchGifs").click(function (event) {
 
         //prevents page from reloading on ajax call
-        event.preventDefault(); 
+        //event.preventDefault(); 
         
         var url = "http://api.giphy.com/v1/gifs/search?";
         var params = $("#searchForm").serialize(); 
-        var req = url + params + "&limit=8";
+        var req = url + params;
         console.log("calling... " + req );
 
         // Call to server
@@ -22,11 +22,14 @@ $(document).ready(function () {
         .done( function(response) {
             console.log("Response data:", response);
             var gifs = response.data;
+            var numGifs = response.pagination.count;
             var gif = "";
             for (i in gifs){
-                gif += '<img class="mediaImg" src="'+ gifs[i].images.original.url +'" alt="' + gifs[i].title + '"/>\n';
+                gif += '<a target="_blank" href="'+ gifs[i].url +'"><img class="mediaImg" src="'
+                + gifs[i].images.fixed_width_downsampled.url +'" alt="' + gifs[i].title + '"/></a>\n';
             };
-
+            console.log(response.pagination.count);
+            $("#numGifs").text(`Number of GIFs: ${numGifs}`);
             $("#gifOutput").html(gif);
         });
     });
